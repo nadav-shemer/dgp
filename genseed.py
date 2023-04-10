c1de0d9c53ea2434f5e6e590cf3c475ee62c0d70
Nadav Shemer <nadav@shemer.org>
2023-04-10 06:15:34 +0300

    Add english.txt, fix genseed.py to work with python3

--- genseed.py
+++ genseed.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import base64
 import os
 
 def bytes_to_int(bytes_rep):
-    """convert a string of bytes (in big-endian order) to a long integer
+    """convert a string of bytes (in big-endian order) to an integer
 
     :param bytes_rep: the raw bytes
     :type bytes_rep: str
     :return: the unsigned integer
-    :rtype: long
+    :rtype: int
     """
-    return long(base64.b16encode(bytes_rep), 16)
+    return int(base64.b16encode(bytes_rep), 16)
 
 dec_digit_to_base58 = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 #base58_digit_to_dec = { b58:dec for dec,b58 in enumerate(dec_digit_to_base58) }
@@ -25,12 +25,12 @@ def get_xkcd(int_data):
     res = []
     while int_data > 0:
         mod = int_data % 2048
-        int_data = int_data / 2048
+        int_data = int_data // 2048
         res.append(wordlist[mod])
     return res
 
 bin_data = os.urandom(40)
 int_data = bytes_to_int(bin_data)
 words = get_xkcd(int_data)
-print '{} words:'.format(len(words))
-print ' '.join(words)
+print('{} words:'.format(len(words)))
+print( ' '.join(words))
